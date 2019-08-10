0958cc09173021f2dde3e75cf31bac3044e21393
froemero <romy.froemer@gmail.com>
2019-08-10 15:38:38 -0400

    Create addSPM.m
    initial commit

---
+++ addSPM.m
@@ -0,0 +1,8 @@
+function addSPM
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here /Applications/fieldtrip-20170701/external/spm8
+%addpath(genpath('/Users/Romy/Dropbox (Brown)/SPM8prac_ST/spm8'));
+
+addpath(genpath('/Applications/spm12'));
+end
+
